8d0f4c662317cac349ddd097eed40939aebbd4e4
Scott Bailey <Scott@TheBaileyFamily.org>
2023-06-14 14:05:55 -0500

    Reorged this a bit. Added BigInt for llvm

--- main.cpp
+++ main.cpp
@@ -4,143 +4,80 @@
 #include "abseil-cpp/absl/numeric/int128.h"
 #include "intx/intx.hpp"
 
-static void abseil_create_uint128(benchmark::State& state) {
-  for (auto _ : state)
-    absl::uint128 v = 1;
-  
+#include <iostream>
+
+template<typename TYPE>
+static void create_type(benchmark::State& state) {
+   for (auto _ : state)
+      TYPE v = 1;
 }
-BENCHMARK(abseil_create_uint128);
 
-
-static void abseil_add_uint128(benchmark::State& state) {
-  absl::uint128 v = 1;
-  uint64_t a = 999999;
-  for (auto _ : state)
-    v += a;
+template<typename TYPE>
+static void add_type(benchmark::State& state) {
+   TYPE     v = 1;
+   uint64_t a = 999999;
+   for (auto _ : state)
+      v += a;
 }
-BENCHMARK(abseil_add_uint128);
 
-static void abseil_multiply_uint128(benchmark::State& state) {
-  absl::uint128 v = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    v *= a;
+template<typename TYPE>
+static void multiply_type(benchmark::State& state) {
+   TYPE     v = 10;
+   uint64_t a = 2'400;
+   for (auto _ : state)
+      v *= a;
 }
-BENCHMARK(abseil_multiply_uint128);
 
-static void abseil_add_multiply_uint128(benchmark::State& state) {
-  absl::uint128 v = 10;
-  absl::uint128 x = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    x += v * a;
+template<typename TYPE>
+static void add_multiply_type(benchmark::State& state) {
+   TYPE     v = 10;
+   TYPE     x = 10;
+   uint64_t a = 2'400;
+   for (auto _ : state)
+      x += v * a;
 }
-BENCHMARK(abseil_add_multiply_uint128);
-
-///////////////////////
-
-static void builtin_create_uint128(benchmark::State& state) {
-  for (auto _ : state)
-    unsigned __int128 v = 1;
-  
-}
-BENCHMARK(builtin_create_uint128);
 
 
-static void builtin_add_uint128(benchmark::State& state) {
-  unsigned __int128 v = 1;
-  uint64_t a = 999999;
-  for (auto _ : state)
-    v += a;
-}
-BENCHMARK(builtin_add_uint128);
 
-static void builtin_multiply_uint128(benchmark::State& state) {
-  unsigned __int128 v = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    v *= a;
-}
-BENCHMARK(builtin_multiply_uint128);
+#define BENCHMARK_TYPE(TYPE)                           \
+   BENCHMARK( create_type<TYPE> );                     \
+   BENCHMARK( add_type<TYPE> );                        \
+   BENCHMARK( multiply_type<TYPE> );                   \
+   BENCHMARK( add_multiply_type<TYPE> );               \
+   /* BENCHMARK_TYPE */
 
-static void builtin_add_multiply_uint128(benchmark::State& state) {
-  unsigned __int128 v = 10;
-  unsigned __int128 x = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    x += v * a;
-}
-BENCHMARK(builtin_add_multiply_uint128);
+/*
+BENCHMARK_TYPE( absl::uint128 );
+BENCHMARK_TYPE( unsigned __int128 );
+BENCHMARK_TYPE( intx::uint128 );
+BENCHMARK_TYPE( boost::multiprecision::uint128_t );
+#if __llvm__
+  BENCHMARK_TYPE( _BitInt );
+#endif
+*/
 
 
-//////
 
-static void intx_create_uint128(benchmark::State& state) {
-  for (auto _ : state)
-    intx::uint128 v = 1;
-  
-}
-BENCHMARK(intx_create_uint128);
+#if __llvm__
+# define BENCHMARK_BIGINT(FUNC) BENCHMARK( FUNC< _BitInt(128) > )
+#else
+# define BENCHMARK_BIGINT(FUNC)
+#endif
 
-
-static void intx_add_uint128(benchmark::State& state) {
-  intx::uint128 v = 1;
-  uint64_t a = 999999;
-  for (auto _ : state)
-    v += a;
-}
-BENCHMARK(intx_add_uint128);
-
-static void intx_multiply_uint128(benchmark::State& state) {
-  intx::uint128 v = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    v *= a;
-}
-BENCHMARK(intx_multiply_uint128);
-
-static void intx_add_multiply_uint128(benchmark::State& state) {
-  intx::uint128 v = 10;
-  intx::uint128 x = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    x += v * a;
-}
-BENCHMARK(intx_add_multiply_uint128);
-
-////
-
-static void boost_create_uint128(benchmark::State& state) {
-  for (auto _ : state)
-    boost::multiprecision::uint128_t v = 1;
-  
-}
-BENCHMARK(boost_create_uint128);
+#define BENCHMARK_FUNC(FUNC)                                \
+   BENCHMARK( FUNC< boost::multiprecision::uint128_t > );   \
+   BENCHMARK( FUNC< intx::uint128 > );                      \
+   BENCHMARK( FUNC< absl::uint128 > );                      \
+   BENCHMARK( FUNC< unsigned __int128> );                   \
+   BENCHMARK_BIGINT( FUNC );                                \
+   /* BENCHMARK_FUNC */
 
 
-static void boost_add_uint128(benchmark::State& state) {
-  boost::multiprecision::uint128_t v = 1;
-  uint64_t a = 999999;
-  for (auto _ : state)
-    v += a;
-}
-BENCHMARK(boost_add_uint128);
+BENCHMARK_FUNC(create_type)
+BENCHMARK_FUNC(add_type)
+BENCHMARK_FUNC(multiply_type)
+BENCHMARK_FUNC(add_multiply_type)
 
-static void boost_multiply_uint128(benchmark::State& state) {
-  boost::multiprecision::uint128_t v = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    v *= a;
-}
-BENCHMARK(boost_multiply_uint128);
 
-static void boost_add_multiply_uint128(benchmark::State& state) {
-  boost::multiprecision::uint128_t v = 10;
-  boost::multiprecision::uint128_t x = 10;
-  uint64_t a = 2'400;
-  for (auto _ : state)
-    x += v * a;
-}
-BENCHMARK(boost_add_multiply_uint128);
 
 BENCHMARK_MAIN();
